e9751a2418d1bafa617618a4b34bd3469093e8a5
Mike  Mwanga <mikemwanga@mikemwanga.local>
2022-06-11 11:21:02 +0300

    updated repo

--- scripts/extract_coordinates.sh
+++
@@ -1,7 +0,0 @@
-#COPYRIGHT 2022
-#Created by Mike Mwanga
-#mikemwanga6@gmail.com
-#motivation from this link
-
-#extract the sequence id, and start and stop positions
-awk -F "\t" '{OFS="\t"}{print $2,$9,$10}' results.out > hits.txt
